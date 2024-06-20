b8d329da061707ee52bb9bd602929b5790330eb2
LeonardSuares <leonard.suares@gmail.com>
2024-06-20 14:39:43 -0400

    Add project files.

---
+++ SimpleQuizApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SimpleQuizApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
